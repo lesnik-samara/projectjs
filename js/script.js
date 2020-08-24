38dd9a2f54b6ed973e1490631a95b8cd9ed5dbf2
Rust <lesnik-samara@mail.ru>
2020-08-24 18:56:37 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// const result = confirm('Are you here?')
+// console.log(result);
+
+// const answer = prompt("Вам есть 19?", "12"); 
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('как дела?', '');
+// answers[1] = prompt('сколько лет?', '');
+// answers[2] = prompt('всё?', '');
+
+// document.write(answers);
+
+const category = "toys";
+
+console.log(`http://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++, --decr);
+
+ 
